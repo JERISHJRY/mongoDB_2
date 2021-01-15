f4a81e14652c5d4833534c5a1e38e4874f7e2c86
JERISHJRY <jerish@techversantinfo.com>
2021-01-15 17:08:36 +0530

    First Commit

---
+++ database/schemas/posts.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+    title: {
+        type : String,
+        required : true
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    date : {
+        type : Date,
+        default : Date.now
+    },
+})
+
+const postsModel = mongoose.model("Posts",postSchema);
+
+module.exports =  postsModel;
